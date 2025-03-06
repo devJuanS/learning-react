f098ff4e41ce28cd6012141de4fa4ff08644aad5
Juan Sebastian Echeverry Parada <jsebast32@gmail.com>
2025-03-06 14:38:56 -0500

    add the FollowMouse component using React Effects

--- README.md
+++ README.md
@@ -9,4 +9,4 @@ Here is the projects done during the courses about React.
 |  ##  | Project        | Topic                                       |                    Code                     |    Web    |
 | :--: | :------------- | :------------------------------------------ | :-----------------------------------------: | :-------: |
 | `02` | Tic tac toe    | `component` `JSX` `useState` `localStorage` |  [View](courses-projects/02-tic-tac-toe/)   | _pending_ |
-| `03` | Mouse follower | `useEffect`                                 | [View](courses-projects/03-mouse-follower/) | _pending_ |
+| `03` | Mouse follower | `useEffect` `clean up effect`               | [View](courses-projects/03-mouse-follower/) | _pending_ |
--- courses-projects/03-mouse-follower/src/App.jsx
+++ courses-projects/03-mouse-follower/src/App.jsx
@@ -1,9 +1,68 @@
+import { useEffect, useState } from "react";
+
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({x: 0, y: 0});
+
+  useEffect(()=>{
+    console.log(`effect: mouse follower ${ enabled ? 'activated' : 'deactivated'}.`);
+
+    const handlePointerMove = (event) => {
+      const { clientX, clientY } = event;
+      setPosition({ x: clientX, y: clientY });
+    }
+    
+    if ( enabled ) {
+      window.addEventListener('pointermove', handlePointerMove);
+    }
+
+    return () => {
+      console.log('clean up effect');
+      setPosition({ x: 0, y: 0 });
+      window.removeEventListener('pointermove', handlePointerMove);
+    }
+  }, [enabled]);
+
+  // hide the pointer to only show the circle
+  useEffect(() => {
+    document.body.classList.toggle('no-cursor', enabled);
+
+    return () => {
+      document.body.classList.remove('no-cursor');
+    }
+  }, [enabled]);
+
+  const handleButtonClick = () => setEnabled(!enabled);
+
+  return (
+    <>
+      <div style={{
+        position: 'absolute',
+        top: -15,
+        left: -15,
+        height: 30,
+        width: 30,
+        border: '1px solid #555',
+        borderRadius: '50%',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        transform: `translate(${ position.x }px, ${ position.y }px)`
+      }}
+      />
+      <button onClick={ handleButtonClick }>
+        { enabled ? 'Deactivate' : 'Activate'} mouse follower
+      </button>
+    </>
+  );
+}
 
 function App() {
 
   return (
     <>
       <h2>Mouse Follower</h2>
+      <FollowMouse />
     </>
   )
 }
